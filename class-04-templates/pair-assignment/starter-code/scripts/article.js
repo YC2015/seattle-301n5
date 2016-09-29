d045e4a31567113e35a4748a6dd2dfcc9aa9bfac
YC <yamile.colque@gmail.com>
2016-09-28 20:48:55 -0700

    working code

--- class-04-templates/pair-assignment/starter-code/scripts/article.js
+++ class-04-templates/pair-assignment/starter-code/scripts/article.js
@@ -15,7 +15,9 @@ Article.prototype.toHtml = function() {
   // TODO: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-
+  var source = $('#template-script').html();
+  var template = Handlebars.compile(source);
+  // $('.template').append(template(this));
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -24,6 +26,8 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+  ;
+  return template(this);
 };
 
 rawData.sort(function(a,b) {
